bc26ef816a960003067dbd34417ba8df08f6144c
stephanHAWK <stephanalvesdias@yahoo.de>
2024-01-02 19:01:02 +0100

    release

---
+++ build-MP3-Player-Desktop_Qt_5_15_2_MinGW_64_bit-Release/MP3-Player_autogen/EWIEGA46WW/moc_mainwindow.cpp
@@ -0,0 +1,262 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../../Lufthansa_MP3-Player/MP3-Player/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[37];
+    char stringdata0[949];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 34), // "on_pushButton_addDirectory_cl..."
+QT_MOC_LITERAL(2, 46, 0), // ""
+QT_MOC_LITERAL(3, 47, 29), // "on_pushButton_addSong_clicked"
+QT_MOC_LITERAL(4, 77, 32), // "on_pushButton_removeSong_clicked"
+QT_MOC_LITERAL(5, 110, 36), // "on_pushButton_addSongToQueue_..."
+QT_MOC_LITERAL(6, 147, 8), // "loadData"
+QT_MOC_LITERAL(7, 156, 45), // "on_horizontalSlider_songProgr..."
+QT_MOC_LITERAL(8, 202, 5), // "value"
+QT_MOC_LITERAL(9, 208, 46), // "on_horizontalSlider_songProgr..."
+QT_MOC_LITERAL(10, 255, 47), // "on_horizontalSlider_songProgr..."
+QT_MOC_LITERAL(11, 303, 44), // "on_horizontalSlider_volumeLev..."
+QT_MOC_LITERAL(12, 348, 45), // "on_horizontalSlider_volumeLev..."
+QT_MOC_LITERAL(13, 394, 46), // "on_horizontalSlider_volumeLev..."
+QT_MOC_LITERAL(14, 441, 13), // "setVolumeIcon"
+QT_MOC_LITERAL(15, 455, 8), // "position"
+QT_MOC_LITERAL(16, 464, 32), // "on_pushButton_fullscreen_clicked"
+QT_MOC_LITERAL(17, 497, 14), // "showNormalized"
+QT_MOC_LITERAL(18, 512, 29), // "on_pushButton_shuffle_clicked"
+QT_MOC_LITERAL(19, 542, 34), // "on_pushButton_previousSong_cl..."
+QT_MOC_LITERAL(20, 577, 25), // "onDoubleClickTimerTimeout"
+QT_MOC_LITERAL(21, 603, 26), // "on_pushButton_play_clicked"
+QT_MOC_LITERAL(22, 630, 8), // "playSong"
+QT_MOC_LITERAL(23, 639, 30), // "on_pushButton_nextSong_clicked"
+QT_MOC_LITERAL(24, 670, 8), // "nextSong"
+QT_MOC_LITERAL(25, 679, 11), // "playNewSong"
+QT_MOC_LITERAL(26, 691, 28), // "on_pushButton_repeat_clicked"
+QT_MOC_LITERAL(27, 720, 28), // "on_pushButton_volume_clicked"
+QT_MOC_LITERAL(28, 749, 26), // "on_tableWidget_itemClicked"
+QT_MOC_LITERAL(29, 776, 17), // "QTableWidgetItem*"
+QT_MOC_LITERAL(30, 794, 4), // "item"
+QT_MOC_LITERAL(31, 799, 32), // "on_tableWidget_itemDoubleClicked"
+QT_MOC_LITERAL(32, 832, 25), // "tableWidget_setItemsFlags"
+QT_MOC_LITERAL(33, 858, 17), // "handleItemEntered"
+QT_MOC_LITERAL(34, 876, 29), // "handleHorizontalHeaderClicked"
+QT_MOC_LITERAL(35, 906, 12), // "logicalIndex"
+QT_MOC_LITERAL(36, 919, 29) // "setOldSelectedCellTransparent"
+
+    },
+    "MainWindow\0on_pushButton_addDirectory_clicked\0"
+    "\0on_pushButton_addSong_clicked\0"
+    "on_pushButton_removeSong_clicked\0"
+    "on_pushButton_addSongToQueue_clicked\0"
+    "loadData\0on_horizontalSlider_songProgress_valueChanged\0"
+    "value\0on_horizontalSlider_songProgress_sliderPressed\0"
+    "on_horizontalSlider_songProgress_sliderReleased\0"
+    "on_horizontalSlider_volumeLevel_valueChanged\0"
+    "on_horizontalSlider_volumeLevel_sliderPressed\0"
+    "on_horizontalSlider_volumeLevel_sliderReleased\0"
+    "setVolumeIcon\0position\0"
+    "on_pushButton_fullscreen_clicked\0"
+    "showNormalized\0on_pushButton_shuffle_clicked\0"
+    "on_pushButton_previousSong_clicked\0"
+    "onDoubleClickTimerTimeout\0"
+    "on_pushButton_play_clicked\0playSong\0"
+    "on_pushButton_nextSong_clicked\0nextSong\0"
+    "playNewSong\0on_pushButton_repeat_clicked\0"
+    "on_pushButton_volume_clicked\0"
+    "on_tableWidget_itemClicked\0QTableWidgetItem*\0"
+    "item\0on_tableWidget_itemDoubleClicked\0"
+    "tableWidget_setItemsFlags\0handleItemEntered\0"
+    "handleHorizontalHeaderClicked\0"
+    "logicalIndex\0setOldSelectedCellTransparent"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      30,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  164,    2, 0x08 /* Private */,
+       3,    0,  165,    2, 0x08 /* Private */,
+       4,    0,  166,    2, 0x08 /* Private */,
+       5,    0,  167,    2, 0x08 /* Private */,
+       6,    0,  168,    2, 0x08 /* Private */,
+       7,    1,  169,    2, 0x08 /* Private */,
+       9,    0,  172,    2, 0x08 /* Private */,
+      10,    0,  173,    2, 0x08 /* Private */,
+      11,    1,  174,    2, 0x08 /* Private */,
+      12,    0,  177,    2, 0x08 /* Private */,
+      13,    0,  178,    2, 0x08 /* Private */,
+      14,    1,  179,    2, 0x08 /* Private */,
+      16,    0,  182,    2, 0x08 /* Private */,
+      17,    0,  183,    2, 0x08 /* Private */,
+      18,    0,  184,    2, 0x08 /* Private */,
+      19,    0,  185,    2, 0x08 /* Private */,
+      20,    0,  186,    2, 0x08 /* Private */,
+      21,    0,  187,    2, 0x08 /* Private */,
+      22,    0,  188,    2, 0x08 /* Private */,
+      23,    0,  189,    2, 0x08 /* Private */,
+      24,    0,  190,    2, 0x08 /* Private */,
+      25,    0,  191,    2, 0x08 /* Private */,
+      26,    0,  192,    2, 0x08 /* Private */,
+      27,    0,  193,    2, 0x08 /* Private */,
+      28,    1,  194,    2, 0x08 /* Private */,
+      31,    1,  197,    2, 0x08 /* Private */,
+      32,    0,  200,    2, 0x08 /* Private */,
+      33,    1,  201,    2, 0x08 /* Private */,
+      34,    1,  204,    2, 0x08 /* Private */,
+      36,    0,  207,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   15,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 29,   30,
+    QMetaType::Void, 0x80000000 | 29,   30,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 29,   30,
+    QMetaType::Void, QMetaType::Int,   35,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_addDirectory_clicked(); break;
+        case 1: _t->on_pushButton_addSong_clicked(); break;
+        case 2: _t->on_pushButton_removeSong_clicked(); break;
+        case 3: _t->on_pushButton_addSongToQueue_clicked(); break;
+        case 4: _t->loadData(); break;
+        case 5: _t->on_horizontalSlider_songProgress_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->on_horizontalSlider_songProgress_sliderPressed(); break;
+        case 7: _t->on_horizontalSlider_songProgress_sliderReleased(); break;
+        case 8: _t->on_horizontalSlider_volumeLevel_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->on_horizontalSlider_volumeLevel_sliderPressed(); break;
+        case 10: _t->on_horizontalSlider_volumeLevel_sliderReleased(); break;
+        case 11: _t->setVolumeIcon((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->on_pushButton_fullscreen_clicked(); break;
+        case 13: _t->showNormalized(); break;
+        case 14: _t->on_pushButton_shuffle_clicked(); break;
+        case 15: _t->on_pushButton_previousSong_clicked(); break;
+        case 16: _t->onDoubleClickTimerTimeout(); break;
+        case 17: _t->on_pushButton_play_clicked(); break;
+        case 18: _t->playSong(); break;
+        case 19: _t->on_pushButton_nextSong_clicked(); break;
+        case 20: _t->nextSong(); break;
+        case 21: _t->playNewSong(); break;
+        case 22: _t->on_pushButton_repeat_clicked(); break;
+        case 23: _t->on_pushButton_volume_clicked(); break;
+        case 24: _t->on_tableWidget_itemClicked((*reinterpret_cast< QTableWidgetItem*(*)>(_a[1]))); break;
+        case 25: _t->on_tableWidget_itemDoubleClicked((*reinterpret_cast< QTableWidgetItem*(*)>(_a[1]))); break;
+        case 26: _t->tableWidget_setItemsFlags(); break;
+        case 27: _t->handleItemEntered((*reinterpret_cast< QTableWidgetItem*(*)>(_a[1]))); break;
+        case 28: _t->handleHorizontalHeaderClicked((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 29: _t->setOldSelectedCellTransparent(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 30)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 30;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 30)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 30;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
